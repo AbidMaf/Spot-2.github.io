3f44f5189d50c7e12d438b2e84c4e95f47b04515
Adrian S <80952805+a-s-wijaya@users.noreply.github.com>
2022-03-28 17:29:40 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function setdata() {
+    var nim = document.getElementById("nim").value;
+    var password = document.getElementById("password").value;
+
+    localStorage.setItem("setnim", nim);
+    localStorage.setItem("setpass", password);
+}
+
+function getdata(){
+    document.getElementById("getnim").innerHTML = localStorage.getItem("setnim");
+    document.getElementById("getpass").innerHTML = localStorage.getItem("setpass");
+}
