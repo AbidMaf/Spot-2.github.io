80ab4549fed16aaffb663ab32790567275eedb34
a-s-wijaya <adrian.s.wijaya@upi.edu>
2022-03-29 12:30:13 +0700

    Update Js + Identity
    Login sekarang udah verif by nim, jadi kalo nimnya sesuai sama yang ada dia bakalan pake nama kalian

--- script.js
+++ script.js
@@ -7,5 +7,18 @@ function setdata() {
 }
 
 function getdata(){
-    document.getElementById("getnim").innerHTML = localStorage.getItem("setnim");
+    document.getElementById("getnim").textContent = localStorage.getItem("setnim");
+
+    var nim = document.getElementById("getnim");
+    
+    if (nim.textContent == "2000637"){
+        document.getElementById("getnama").textContent="Adrian Sugandi Wijaya";
+    }else if (nim.textContent == "2000649"){
+        document.getElementById("getnama").textContent="Abid Mafahim";
+    }else if (nim.textContent == "2000427"){
+        document.getElementById("getnama").textContent="Faaris Muda Dwi Nugraha";
+    }else{
+        document.getElementById("getnama").textContent="Nama tidak ditemukan";
+        document.getElementById("getnim").textContent="NIM tidak ditemukan";
+    }
 }
