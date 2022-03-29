027803d5b1c6d44c0c90719d2c2560522e3e8d30
AbidMaf <80903278+AbidMaf@users.noreply.github.com>
2022-03-29 17:35:54 +0700

    Login Function Update

--- script.js
+++ script.js
@@ -2,8 +2,25 @@ function setdata() {
     var nim = document.getElementById("nim").value;
     var password = document.getElementById("password").value;
 
-    localStorage.setItem("setnim", nim);
-    localStorage.setItem("setpass", password);
+    const nimList = ['2000649', '2000427', '2000637'];
+    const passList = ['1234abcd', 'abcd1234', 'aaaa0000'];
+
+    var checkNim = nimList.includes(nim);
+    if(checkNim == true){
+        var nimIndex = nimList.indexOf(nim);
+        if(passList[nimIndex] == password){
+            localStorage.setItem("setnim", nim);
+            localStorage.setItem("setpass", password);
+            location.replace("dashboard.html");
+        }
+        else{
+            alert("NIM atau password salah");
+        }
+    }
+    else{
+        alert("NIM atau password salah");
+    }
+
 }
 
 function getdata(){
