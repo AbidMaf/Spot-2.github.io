bc4c5eb1819b48c6781d0bbb9ebc04ea769083c9
AbidMaf <80903278+AbidMaf@users.noreply.github.com>
2022-03-31 09:01:29 +0700

    Minor change on login and sidebar

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 function setdata() {
     var nim = document.getElementById("nim").value;
     var password = document.getElementById("password").value;
-
+    
     const nimList = [
         '2000649', '2000427', '2000637', 'guest'
     ];
     const passList = [
         '1234abcd', 'abcd1234', 'aaaa0000', 'guest'
     ];
-
+    
     var checkNim = nimList.includes(nim);
-    if(checkNim == true){
-        var nimIndex = nimList.indexOf(nim);
-        if(passList[nimIndex] == password){
-            localStorage.setItem("setnim", nim);
-            localStorage.setItem("setpass", password);
-            location.replace("dashboard.html");
+    if (nim != '' || password != '') {
+        console.log("nim: " + nim + " pass: "+ password);
+        if(checkNim == true){
+            var nimIndex = nimList.indexOf(nim);
+            if(passList[nimIndex] == password){
+                localStorage.setItem("setnim", nim);
+                localStorage.setItem("setpass", password);
+                location.replace("dashboard.html");
+            }
+            else{
+                alert("NIM atau password salah");
+            }
         }
         else{
             alert("NIM atau password salah");
         }
+    } else {
+        alert("Semua masukan harus terisi");
     }
-    else{
-        alert("NIM atau password salah");
-    }
-
 }
 
 function getdata(){
     document.getElementById("getnim").textContent = localStorage.getItem("setnim");
-
+    
     var nim = document.getElementById("getnim");
     
     if (nim.textContent == "2000637"){
