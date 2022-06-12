a2384cba52bebaa741c76f5cf27708ea8f16f288
Faaris Muda <faarismudadwinugraha@gmail.com>
2022-06-12 23:47:07 +0700

    Update koreksipbo.php

--- App/Controller/koreksipbo.php
+++ App/Controller/koreksipbo.php
@@ -3,7 +3,7 @@
       $nilai = $_POST['nilai'];
       $kd_matkul = "RL209";
       $id_up_tugas = $_POST['id_up_tugas'];
-      $sql = "UPDATE upload_tugas SET nilai='$nilai' WHERE npm='$nim' && kd_matkul='$kd_matkul' && id_tugas='$id_tugas'";
+      $sql = "UPDATE upload_tugas SET nilai='$nilai' WHERE npm='$nim' && kd_matkul='$kd_matkul' && id_up_tugas='$id_up_tugas'";
      if (mysqli_query($conn, $sql)) {
       echo ("<script LANGUAGE='JavaScript'>
          window.alert('Data Berhasil Diupdate');
